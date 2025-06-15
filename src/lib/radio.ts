24c1cb651e309e47b960d49738dbe540e2734458
mOqOm <moqom.mopom@gmail.com>
2025-06-15 22:02:40 +0900

    一時コミット

--- src/lib/radio.ts
+++ src/lib/radio.ts
@@ -1,31 +1,42 @@
 import express, { Application, Request, Response } from 'express';
 import cron from 'node-cron';
-import IcyMetadata from 'icy-metadata';
-import { capitalize } from 'lodash';
 import RdkProg from './prog';
 import Radiko from './radiko';
-import type { BrowseItem, BrowseList, BrowseResult} from './models/BrowseResultModel';
-import libQ from 'kew';
+import type { BrowseItem, BrowseList, BrowseResult } from './models/BrowseResultModel';
+
+import { DELAY_sec, getCurrentRadioTime, formatTimeString, getTimeSpan } from './radioTime';
+
 
 export default class JpRadio {
   private readonly app: Application;
   private server: ReturnType<Application['listen']> | null = null;
-  private readonly task: ReturnType<typeof cron.schedule>;
+  private readonly task1: ReturnType<typeof cron.schedule>;
+  private readonly task2: ReturnType<typeof cron.schedule>;
   private readonly port: number;
   private readonly logger: Console;
   private readonly acct: any;
   private readonly commandRouter: any;
   private prg: RdkProg | null = null;
   private rdk: Radiko | null = null;
+  private station: string = '';
+  private task2_cnt: number = 0;
 
-  constructor(port = 0, logger: Console, acct: any = null, commandRouter: any) {
+  private readonly serviceName: any;
+
+  constructor(port = 0, logger: Console, acct: any = null, commandRouter: any, serviceName: any) {
     this.app = express();
     this.port = port;
     this.logger = logger;
     this.acct = acct;
     this.commandRouter = commandRouter;
+    this.serviceName = serviceName;
 
-    this.task = cron.schedule('0 3,9,15 * * *', this.#pgupdate.bind(this), {
+    // 番組表データ更新（6h間隔）
+    this.task1 = cron.schedule('0 5,11,17,23 * * *', this.#pgupdate.bind(this), {
+      scheduled: false
+    });
+    // 再生画面更新（60s間隔;getCurrentRadioTimeに対して1sずらし）
+    this.task2 = cron.schedule(`${DELAY_sec + 1} * * * * *`, this.#pushSongState.bind(this), {
       scheduled: false
     });
 
@@ -33,24 +44,37 @@ export default class JpRadio {
   }
 
   #setupRoutes(): void {
+    this.logger.info('JP_Radio::JpRadio.#setupRoutes');
+
     this.app.get('/radiko/:stationID', async (req: Request, res: Response): Promise<void> => {
       const station = req.params['stationID'];
+      this.logger.info(`JP_Radio::JpRadio.#setupRoutes.get=> req.originalUrl=${req.originalUrl}`);
 
-      if (!this.rdk || !this.rdk.stations?.has(station)) {
+      if (!this.rdk || !this.rdk.stations?.has(this.station)) {
         const msg = !this.rdk
           ? 'JP_Radio::Radiko instance not initialized'
-          : `JP_Radio::${station} not in available stations`;
+          : `JP_Radio::${this.station} not in available stations`;
         this.logger.error(msg);
         res.status(500).send(msg);
         return;
       }
 
+      this.#startStream(res)
+    });
+
+    this.app.get('/radiko/', (_req, res) => {
+      res.send("Hello, world. You're at the radiko_app index.");
+    });
+  }
+  async #startStream(res: Response): Promise<void> {
+    this.logger.info('JP_Radio::JpRadio.#startStream');
+    if (this.rdk) {
       try {
-        const icyMetadata = new IcyMetadata();
-        const ffmpeg = await this.rdk.play(station);
+        //const icyMetadata = new IcyMetadata();
+        const ffmpeg = await this.rdk.play(this.station);
 
         if (!ffmpeg || !ffmpeg.stdout) {
-          this.logger.error('JP_Radio::ffmpeg start failed or stdout is null');
+          this.logger.error('JP_Radio::JpRadio.#startStream: ffmpeg start failed or stdout is null');
           res.status(500).send('Stream start error');
           return;
         }
@@ -58,49 +82,78 @@ export default class JpRadio {
         let ffmpegExited = false;
         ffmpeg.on('exit', () => {
           ffmpegExited = true;
-          this.logger.debug(`ffmpeg process ${ffmpeg.pid} exited.`);
+          this.logger.debug(`JP_Radio::JpRadio.#startStream: ffmpeg process ${ffmpeg.pid} exited.`);
         });
-
-        const progData = await this.prg?.getCurProgram(station);
-        if (progData) {
-          const title = `${progData.pfm || ''} - ${progData.title || ''}`;
-          icyMetadata.setStreamTitle(title);
-        }
-
-        res.set({
-          'Cache-Control': 'no-cache, no-store',
-          'icy-name': await this.rdk.getStationAsciiName(station),
-          'icy-metaint': icyMetadata.metaInt,
-          'Content-Type': 'audio/aac',
-          Connection: 'keep-alive'
-        });
-
-        ffmpeg.stdout.pipe(icyMetadata).pipe(res);
+        ffmpeg.stdout.pipe(res);
+        this.logger.info(`JP_Radio::JpRadio.#startStream: ffmpeg=${ffmpeg.pid}`);
+        // max60sも待ちたくないのですぐ呼ぶ
+        setTimeout(this.#pushSongState.bind(this), 3000);
+        this.task2.start();
 
         res.on('close', () => {
+          this.task2.stop();
+          this.logger.info('JP_Radio::JpRadio.#startStream: res.on(close)');
           if (ffmpeg.pid && !ffmpegExited) {
             try {
               process.kill(-ffmpeg.pid, 'SIGTERM');
-              this.logger.info(`SIGTERM sent to ffmpeg group ${ffmpeg.pid}`);
+              this.logger.info(`JP_Radio::JpRadio.#startStream: SIGTERM sent to ffmpeg group ${ffmpeg.pid}`);
             } catch (e: any) {
-              this.logger.warn(`Kill ffmpeg failed: ${e.code === 'ESRCH' ? 'Already exited' : e.message}`);
+              this.logger.warn(`JP_Radio::JpRadio.#startStream: Kill ffmpeg failed: ${e.code === 'ESRCH' ? 'Already exited' : e.message}`);
             }
           }
         });
+        this.logger.info('JP_Radio::JpRadio.#startStream: Streaming started');
 
-        this.logger.info('JP_Radio::Streaming started');
       } catch (err) {
-        this.logger.error('JP_Radio::Stream error', err);
+        this.logger.error('JP_Radio::JpRadio.#startStream: Stream error', err);
         res.status(500).send('Internal server error');
       }
-    });
-
-    this.app.get('/radiko/', (_req, res) => {
-      res.send("Hello, world. You're at the radiko_app index.");
-    });
+    }
   }
 
-  radioStations(): Promise<BrowseResult> {
+
+  async #pushSongState(): Promise<void> {
+    const state = this.commandRouter.stateMachine.getState();
+    // 番組の切り替わりで更新
+    if (state.seek >= state.duration * 1000 || --this.task2_cnt <= 0) {
+      this.task2_cnt = 10;  // 念のため10分間隔で強制更新
+      const progData = await this.prg?.getCurProgram(this.station);
+      if (progData) {
+        const stationName = await this.rdk?.getStationName(this.station);
+        const performer = progData.pfm ? ` - ${progData.pfm}` : '';
+        const t0 = formatTimeString(progData.ft);
+        const t1 = formatTimeString(progData.tt);
+        const now = formatTimeString(getCurrentRadioTime());
+        const artist = `${stationName} / ${t0.substr(0, 5)}-${t1.substr(0, 5)}`;
+        this.logger.info(`JP_Radio::JpRadio.#pushSongState: "${artist}", now=${now}`);
+
+        state.title = progData.title + performer;
+        state.artist = artist;
+        state.albumart = progData.img || state.albumart;
+        state.duration = getTimeSpan(t0, t1);        // sec
+        state.seek = getTimeSpan(t0, now) * 1000;  // msec
+
+        // workaround to allow state to be pushed when not in a volatile state
+        const queueItem = this.commandRouter.stateMachine.playQueue.arrayQueue[state.position];
+        queueItem.name = state.title;
+        queueItem.artist = state.artist;
+        queueItem.albumart = state.albumart;
+        queueItem.duration = state.duration;
+
+        // reset volumio internal timer
+        this.commandRouter.stateMachine.currentSeek = state.seek;
+        this.commandRouter.stateMachine.currentSongDuration = state.duration;
+
+        // volumio push state
+        this.commandRouter.servicePushState(state, 'mpd');
+        return;
+
+      }
+    }
+  }
+
+  async radioStations(): Promise<BrowseResult> {
+    this.logger.info('JP_Radio::JpRadio.radioStations');
     const defer = libQ.defer();
 
     if (!this.rdk?.stations) {
@@ -118,7 +171,7 @@ export default class JpRadio {
     }
 
     const entries = Array.from(this.rdk.stations.entries());
-
+    // 地域名ごとにグループ化
     const grouped: Record<string, BrowseItem[]> = {};
 
     const stationPromises = entries.map(async ([stationId, stationInfo]) => {
@@ -126,8 +179,9 @@ export default class JpRadio {
         const progData = await this.prg?.getCurProgram(stationId);
 
         const item: BrowseItem = {
-          service: 'webradio',
-          type: 'webradio',
+          // explodeUriを呼び出す先のサービス名
+          service: this.serviceName,
+          type: 'song',
           // 番組タイトル
           title: progData ? `${progData.title || ''}` : '',
           // 地域名 / 局名
@@ -180,15 +234,21 @@ export default class JpRadio {
   }
 
   async start(): Promise<void> {
+    this.logger.info(`JP_Radio::JpRadio.start`);
     if (this.server) {
-      this.logger.info('JP_Radio::Already started');
+      this.logger.info('JP_Radio::JpRadio.start: Already started');
       this.commandRouter.pushToastMessage('info', 'JP Radio', 'すでに起動しています');
       return;
     }
 
     this.prg = new RdkProg(this.logger);
     this.rdk = new Radiko(this.port, this.logger, this.acct);
-    await this.#init();
+    // ここで時間かかり過ぎて，
+    //   Plugin music_service jp_radio failed to complete 'onStart' in a timely fashion
+    // って怒られるので，awaitを外してみた。
+    // BOOTは早くなるし問題なさそうなのでこれでいいんじゃない？
+    //await this.#init();
+    this.#init();
 
     return new Promise((resolve, reject) => {
       this.server = this.app
@@ -197,11 +257,11 @@ export default class JpRadio {
           this.commandRouter.pushToastMessage('success', 'JP Radio', '起動しました');
           this.commandRouter.servicePushState({
             status: 'play',
-            service: 'jp_radio',
+            service: this.serviceName,
             title: 'Radiko 起動中',
             uri: ''
           });
-          this.task.start();
+          this.task1.start();
           resolve();
         })
         .on('error', (err: any) => {
@@ -214,7 +274,8 @@ export default class JpRadio {
 
   async stop(): Promise<void> {
     if (this.server) {
-      this.task.stop();
+      this.task1.stop();
+      this.task2.stop();
       this.server.close();
       this.server = null;
 
@@ -227,13 +288,128 @@ export default class JpRadio {
   }
 
   async #init(): Promise<void> {
+    this.logger.info('JP_Radio::JpRadio.#init');
     if (this.rdk) await this.rdk.init(this.acct);
-    await this.#pgupdate();
+    await this.#pgupdate(true);
   }
 
-  async #pgupdate(): Promise<void> {
-    this.logger.info('JP_Radio::Updating program listings');
-    await this.prg?.updatePrograms();
-    await this.prg?.clearOldProgram();
+  async #pgupdate(whenBoot = false): Promise<void> {
+    if (this.prg) {
+      this.logger.info('JP_Radio::JpRadio.#pgupdate: Updating program listings...');
+      if (whenBoot)
+        this.commandRouter.pushToastMessage('success', 'JP Radio', '番組データ：取得中...');
+
+      // TODO: 設定画面で取得エリアを絞り込めるようにしたい
+      const areaIDs = Array.from({ length: 47 }, (_, i) => `JP${i + 1}`);
+      //const areaIDs = new Array('JP13', 'JP27') // デバッグ用(東京/大阪だけ)
+
+      const stationsMap = this.rdk ? this.rdk.stations : null;
+      const t0 = new Date();
+      await this.prg.updatePrograms(areaIDs, stationsMap, whenBoot);
+      await this.prg.clearOldProgram();
+      const t1 = new Date();
+      const tw = t1.getTime() - t0.getTime();
+
+      if (whenBoot) {
+        this.commandRouter.pushToastMessage('success', 'JP Radio', `番組データ：取得完了！ ${tw}ms`);
+      }
+
+      this.logger.info(`JP_Radio::JpRadio.#pgupdate: complete. ### ${tw}ms ###`);
+    }
+  }
+
+  //=={工事中}================================================================================
+  async radioStations2(): Promise<BrowseResult> {
+    // どれがいい??
+    //  1. エリア→局→日付→番組表→再生 ⇒ radioStations()を流用できそう
+    //  2. 日付→エリア→局→番組表→再生
+    //  3. [エリア/日付/局]→番組表→再生 ⇒ RADIKOアプリはこれ
+    this.logger.info('JP_Radio::JpRadio.radioStations2');
+    return {
+      navigation: {
+        lists: [{
+          title: 'グループ1',
+          availableListViews: ['grid', 'list'],
+          items: [{
+            service: this.serviceName,
+            type: 'track',
+            title: 'track1',
+            artist: '',
+            albumart: '',
+            icon: 'fa fa-folder-open-o',
+            uri: '',
+            name: '',
+            samplerate: '',
+            bitdepth: 0,
+            channels: 0
+          }, {
+            service: this.serviceName,
+            type: 'track',
+            title: 'track2',
+            artist: '',
+            albumart: '',
+            icon: 'fa fa-folder-open-o',
+            uri: '',
+            name: '',
+            samplerate: '',
+            bitdepth: 0,
+            channels: 0
+          }, {
+            service: this.serviceName,
+            type: 'track',
+            title: 'track3',
+            artist: '',
+            albumart: '',
+            icon: 'fa fa-folder-open-o',
+            uri: '',
+            name: '',
+            samplerate: '',
+            bitdepth: 0,
+            channels: 0
+          }]
+        }, {
+          title: 'グループ2',
+          availableListViews: ['grid', 'list'],
+          items: [{
+            service: this.serviceName,
+            type: 'song',
+            title: 'song1',
+            artist: '',
+            albumart: '',
+            icon: 'fa fa-microphone',
+            uri: '',
+            name: '',
+            samplerate: '',
+            bitdepth: 0,
+            channels: 0
+          }, {
+            service: this.serviceName,
+            type: 'song',
+            title: 'song2',
+            artist: '',
+            albumart: '',
+            icon: 'fa fa-microphone',
+            uri: '',
+            name: '',
+            samplerate: '',
+            bitdepth: 0,
+            channels: 0
+          }, {
+            service: this.serviceName,
+            type: 'song',
+            title: 'song3',
+            artist: '',
+            albumart: '',
+            icon: 'fa fa-microphone',
+            uri: '',
+            name: '',
+            samplerate: '',
+            bitdepth: 0,
+            channels: 0
+          }]
+        }]
+      },
+      uri: 'radiko2'
+    };
   }
 }
