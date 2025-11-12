b350801f150e30da61035bcd539254aef2087212
mOqOm <moqom.mopom@gmail.com>
2025-11-13 00:00:45 +0900

    内部はUTC管理であることをコメントで追加

--- src/models/radiko-program.model.ts
+++ src/models/radiko-program.model.ts
@@ -3,7 +3,9 @@ import { DateTime } from "@/types/date-time.types";
 export interface RadikoProgramData {
   stationId: string;
   progId: string;
+  /** 開始日時(UTC) */
   ft: DateTime;
+  /** 終了日時(UTC) */
   to: DateTime;
   title: string;
   info?: string;
