549e3dbb87d59566abf4864f5fac7c0a170f64f3
mOqOm <moqom.mopom@gmail.com>
2026-07-24 21:03:25 +0900

    非推奨を修正

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,7 @@
-const { pathsToModuleNameMapper } = require('ts-jest');
+const { createDefaultPreset, pathsToModuleNameMapper } = require('ts-jest');
 const { compilerOptions } = require('./tsconfig');
 
 module.exports = {
-  preset: 'ts-jest',
   testEnvironment: 'node',
   // testディレクトリ内の.test.tsを対象
   testMatch: ['**/test/**/*.test.ts'],
@@ -12,11 +11,7 @@ module.exports = {
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/src/' }),
 
   // TypeScript で import/export を扱えるようにする(test/はsrc/の外にあるためrootDir制約のないtsconfig.test.jsonを使う)
-  globals: {
-    'ts-jest': {
-      tsconfig: 'tsconfig.test.json',
-    },
-  },
+  ...createDefaultPreset({ tsconfig: 'tsconfig.test.json' }),
 
   // Node.js 標準のモジュール解決を使う
   moduleFileExtensions: ['ts', 'js', 'json', 'node'],
